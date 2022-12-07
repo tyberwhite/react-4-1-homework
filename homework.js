aeb48a436274d39a2581dbec6c1596f89947ec1d
Jesse <zerocool@Jesses-MBP.hsd1.ma.comcast.net>
2022-12-07 10:13:10 -0500

    created person class

--- homework.js
+++ homework.js
@@ -6,11 +6,13 @@
 // Classes MUST be defined before they can be constructed.
 
 // We don't need keywords like let, const, or var to declare fields
+
+// Create Hamster class. Set name from parameter in constructor method
 class Hamster {
-  owner = "";
-  price = 15;
-  constructor(name) {
+  constructor(owner = "", name, price = 15) {
+    this.owner = owner;
     this.name = name;
+    this.price = price;
   }
 
   wheelRun() {
@@ -26,5 +28,49 @@ class Hamster {
   }
 }
 
-let tori = new Hamster("tori");
-console.log(tori.name);
+// Create Person class
+class Person {
+  constructor(
+    name,
+    age = 0,
+    height = 0,
+    weight = 0,
+    mood = 0,
+    hamsters = [],
+    bankAccount = 0
+  ) {
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+    this.mood = mood;
+    this.hamsters = hamsters;
+    this.bankAccount = bankAccount;
+  }
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+  getWeight() {
+    return this.weight;
+  }
+  greet() {
+    console.log(`Hi! My name is ${this.name}`);
+  }
+  eat() {
+    this.weight++;
+    this.mood++;
+  }
+  exercise() {
+    this.weight--;
+  }
+  ageUp() {
+    this.age++;
+    this.height++;
+    this.weight++;
+    this.mood--;
+    this.bankAccount += 10;
+  }
+}
